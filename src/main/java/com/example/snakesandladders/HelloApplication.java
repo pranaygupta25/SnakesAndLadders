2d8228e4980a80449c499477b511deff6b8cb3f2
Pranay Gupta <pranay20228@iiitd.ac.in>
2021-12-18 12:19:37 +0530

    disabled window resizable property
    application window should not be resized in order to look good with image backgrounds

--- src/main/java/com/example/snakesandladders/HelloApplication.java
+++ src/main/java/com/example/snakesandladders/HelloApplication.java
@@ -13,6 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Snakes and Ladders | CSE201 Project");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
