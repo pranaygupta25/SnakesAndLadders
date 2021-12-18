59b36e8898c38be9fa0acb0f6a9e10abbb4f0142
Pranay Gupta <pranay20228@iiitd.ac.in>
2021-12-19 00:34:50 +0530

    alternating player highlight after every turn implemented
    background is being changed after every turn to indicate the respective player's turn

--- src/main/java/com/example/snakesandladders/GameController.java
+++ src/main/java/com/example/snakesandladders/GameController.java
@@ -3,12 +3,18 @@ package com.example.snakesandladders;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+
+import java.io.FileInputStream;
 
 public class GameController {
 
     // -----------------------------------------------------------------------------------------------------------------
+
+    @FXML
+    private ImageView backgroundBoard;
 
     @FXML
     private Button diceButton;
@@ -44,16 +50,29 @@ public class GameController {
         // Is automatically executed while the application launches
         this.board = new GameBoard(player1mover, player1token, player2mover, player2token, diceButton, diceHolder);
         this.activePlayer = 1;
+        changeBackground(1);
+    }
+
+    private void changeBackground(int player) {
+        String url = "src/main/resources/img/gameBoard/player" + Integer.toString(player) + ".png";
+        try {
+            backgroundBoard.setImage(new Image(new FileInputStream(url)));
+        }
+        catch (Exception e){
+            System.out.println("Background Image Switching Error");
+        }
     }
 
     @FXML
     void movePlayer1(MouseEvent event) {
         // Component responsible for event: player1mover
         new Thread() {
-            @Override public void run() {
+            @Override
+            public void run() {
                 board.play(event, 1);
             }
         }.start();
+        changeBackground(2);
     }
 
     @FXML
@@ -64,6 +83,7 @@ public class GameController {
                 board.play(event, 2);
             }
         }.start();
+        changeBackground(1);
     }
 
     @FXML
@@ -85,11 +105,9 @@ public class GameController {
             @Override public void run() {
                 board.rollDice(activePlayer);
                 if (activePlayer == 1) {
-
                     activePlayer = 2;
                 }
                 else {
-
                     activePlayer = 1;
                 }
             }
