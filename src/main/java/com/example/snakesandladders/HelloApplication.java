d81fa59b217ddf147e06bc3cfb0c85f43467edae
Pranay Gupta <pranay20228@iiitd.ac.in>
2021-12-18 12:24:46 +0530

    added comments in the application class
    Comments for understanding what every JavaFX line does added to the source code.

--- src/main/java/com/example/snakesandladders/HelloApplication.java
+++ src/main/java/com/example/snakesandladders/HelloApplication.java
@@ -10,15 +10,22 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // FXML File Configuration
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        // New scene initialization
         Scene scene = new Scene(fxmlLoader.load());
+        // Application Name Configuration
         stage.setTitle("Snakes and Ladders | CSE201 Project");
+        // Application Window Resize
         stage.setResizable(false);
+        // Setting up the current scene to the stage
         stage.setScene(scene);
+        // Display the stage
         stage.show();
     }
 
     public static void main(String[] args) {
+        // Static method from Application Class of the JavaFX library which is being inherited by this class
         launch();
     }
 }
