3fe2897d02e760fc6f3a8ab53f81cf665284c547
Pranay Gupta <pranay20228@iiitd.ac.in>
2021-12-18 17:01:35 +0530

    JavaFX methods defined
    Code for JavaFX interaction methods defined

--- src/main/java/com/example/snakesandladders/GameController.java
+++ src/main/java/com/example/snakesandladders/GameController.java
@@ -34,41 +34,66 @@ public class GameController {
     private ImageView player2token;
     // On action: moveToken2()
 
+    private GameBoard board;
+
+    private int activePlayer;
+
     // -----------------------------------------------------------------------------------------------------------------
 
     public void initialize() {
         // Is automatically executed while the application launches
-
+        this.board = new GameBoard(player1mover, player1token, player2mover, player2token, diceButton, diceHolder);
+        this.activePlayer = 1;
     }
 
     @FXML
     void movePlayer1(MouseEvent event) {
         // Component responsible for event: player1mover
-
+        new Thread() {
+            @Override public void run() {
+                board.play(event, 1);
+            }
+        }.start();
     }
 
     @FXML
     void movePlayer2(MouseEvent event) {
         // Component responsible for event: player2mover
-
+        new Thread() {
+            @Override public void run() {
+                board.play(event, 2);
+            }
+        }.start();
     }
 
     @FXML
     void moveToken1(MouseEvent event) {
         // Component responsible for event: player1token
-
+        board.moveTokenByOne(event, 1);
     }
 
     @FXML
     void moveToken2(MouseEvent event) {
         // Component responsible for event: player2token
-
+        board.moveTokenByOne(event, 2);
     }
 
     @FXML
     void roll(ActionEvent event) {
         // Component responsible for event: diceButton
+        new Thread() {
+            @Override public void run() {
+                board.rollDice(activePlayer);
+                if (activePlayer == 1) {
 
+                    activePlayer = 2;
+                }
+                else {
+
+                    activePlayer = 1;
+                }
+            }
+        }.start();
     }
 
     // -----------------------------------------------------------------------------------------------------------------
