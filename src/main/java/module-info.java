330f010b60d2ba3293fd05b711bd9e4a5b3d9854
Pranay Gupta <pranay20228@iiitd.ac.in>
2021-12-18 12:12:36 +0530

    initialized repository
    Initialized a new Java FX project using IntelliJ IDEA

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.snakesandladders {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.snakesandladders to javafx.fxml;
+    exports com.example.snakesandladders;
+}
