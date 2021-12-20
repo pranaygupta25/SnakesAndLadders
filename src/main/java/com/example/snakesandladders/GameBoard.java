07521572c619515a23c5a9e3adede0cd271ea7dc
Pranay Gupta <pranay20228@iiitd.ac.in>
2021-12-20 18:16:52 +0530

    inherited ladder implementation

--- src/main/java/com/example/snakesandladders/GameBoard.java
+++ src/main/java/com/example/snakesandladders/GameBoard.java
@@ -8,9 +8,9 @@ public class GameBoard {
 
     // -----------------------------------------------------------------------------------------------------------------
 
-    // private Snake[] snakes;
+    // private Snake snakes;
 
-    // private Ladder[] ladders;
+    private Ladder ladders;
 
     private Player[] players;
 
@@ -22,6 +22,8 @@ public class GameBoard {
 
     public GameBoard(ImageView player1mover, ImageView player1token, ImageView player2mover, ImageView player2token,
                      Button diceButton, ImageView diceHolder, ImageView winnerPopup) {
+        int[][] ladders = {{2, 23}, {8, 12}, {17, 93}, {29, 54}, {32, 51}, {39, 80}, {62, 78}, {70, 89}, {75, 96}};
+        this.ladders = new Ladder(ladders);
         this.players = new Player[2];
         this.players[0] = new Player(player1mover, player1token);
         this.players[1] = new Player(player2mover, player2token);
@@ -36,6 +38,10 @@ public class GameBoard {
             this.players[player-1].repeat(event);
         if (this.players[0].getCurrentPosition() == 100 || this.players[1].getCurrentPosition() == 100) {
             winnerPopup.setVisible(true);
+        }
+        if (ladders.isPowerElement(this.players[player-1].getCurrentPosition())){
+            System.out.println(ladders.ladderTopCoordinates(this.players[player-1].getCurrentPosition()));
+
         }
     }
 
--- src/main/java/com/example/snakesandladders/Ladder.java
+++ src/main/java/com/example/snakesandladders/Ladder.java
@@ -1,42 +1,21 @@
 package com.example.snakesandladders;
 
-import java.util.HashMap;
+import java.io.File;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
-public class Ladder {
-    // leader  map  where value[0] is x coordinate and value[1] is y coordinate
-    private HashMap<Integer, int[]> ladder_coordinate = new HashMap<Integer, int[]>(){
-        {
-            put(2, new int[]{100, -150});
-            put(8, new int[]{400,-100});
-            put(17, new int[]{350,-500});
-            put(29, new int[]{300,-300});
-            put(39, new int[]{0,-400});
-            put(32, new int[]{450,-300});
-            put(62, new int[]{100,-400});
-            put(70, new int[]{400,-450});
-            put(75, new int[]{200,-500});
+public class Ladder extends PowerElement {
 
-        }
-    };
-
-    
-
-    // function to give corrdinate where we have to go after taking ladder
-    public int[] getCoordinates(int current) {
-        int[] result = new int[2];
-        result[0] = ladder_coordinate.get(current)[0];
-        result[1] = ladder_coordinate.get(current)[1];
-        return result;
+    public Ladder(int[][] ladderPositions) {
+        super(ladderPositions, "src/main/resources/audio/powerUp.wav");
     }
 
-    // function to check where we are at ladder or not 
-    public boolean isLadder(int current) {
-        return ladder_coordinate.containsKey(current);
-        
+    public int[] ladderTopCoordinates(int bottom) {
+        Media track = new Media(new File(this.url).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(track);
+        mediaPlayer.play();
+        return getCoordinates(bottom);
     }
-
-    // how to use above function if we are at ladder face then call corrdinate fn of ladder to get new postion  to drop player
-
 
 }
 
