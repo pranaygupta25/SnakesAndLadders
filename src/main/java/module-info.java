1f2543285d4a920e8acd3348af1b78431879abde
Pranay Gupta <pranay20228@iiitd.ac.in>
2021-12-20 18:14:25 +0530

    build files injected

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.example.snakesandladders {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires javafx.media;
 
     opens com.example.snakesandladders to javafx.fxml;
     exports com.example.snakesandladders;
